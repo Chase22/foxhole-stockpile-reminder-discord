304af814c701696993d0e601e17edbbdc4ca2593
Chase22 <chase@mailbox.org>
2023-04-13 14:39:50 +0200

    Move hexes to text file

--- src/main/kotlin/de/chasenet/foxhole/domain/Stockpile.kt
+++ src/main/kotlin/de/chasenet/foxhole/domain/Stockpile.kt
@@ -31,42 +31,4 @@ data class Location(
 
 val RESERVATION_EXPIRATION_TIME = 2.days + 4.hours
 
-val HEXES = listOf(
-    "The Fingers",
-    "Great March",
-    "Tempest Island",
-    "Marban Hollow",
-    "ViperPit",
-    "Basin Sionnach",
-    "Dead Lands",
-    "Heartlands",
-    "Endless Shore",
-    "Westgate",
-    "Oarbreaker",
-    "Acrithia",
-    "Mooring County",
-    "Weathered Expanse",
-    "Loch Mor",
-    "Morgens Crossing",
-    "Stonecradle",
-    "Allods Bight",
-    "Kalokai",
-    "Red River",
-    "Origin",
-    "Howl County",
-    "Shackled Chasm",
-    "Speaking Woods",
-    "Terminus",
-    "Linn Mercy",
-    "Clanshead Valley",
-    "Godcrofts",
-    "Nevish Line",
-    "Callums Cape",
-    "Fishermans Row",
-    "Umbral Wildwood",
-    "Reaching Trail",
-    "Callahans Passage",
-    "Ash Fields",
-    "Drowned Vale",
-    "Farranac Coast"
-)
+val HEXES = object{}::class.java.classLoader.getResource("hexes.txt")!!.readText().lines()
